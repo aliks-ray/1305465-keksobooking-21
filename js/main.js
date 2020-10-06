a01fa0959b004b5161aa469c226a9c767552a6c3
Alexandra K <64307537+aliks-ray@users.noreply.github.com>
2020-10-06 17:15:24 +0300

    Правки после проверки

--- js/main.js
+++ js/main.js
@@ -7,17 +7,17 @@ const MAX_X = 1200;
 const PIN_HEIGHT = 70;
 const PIN_WIDTH = 50;
 const AMOUNT_PINS = 8;
-const checkinOptions = [
+const CHEKIN_OPTIONS = [
   `12:00`,
   `13:00`,
   `14:00`
 ];
-const checkoutOptions = [
+const CHEKOUT_OPTIONS = [
   `12:00`,
   `13:00`,
   `14:00`
 ];
-const featuresList = [
+const FEAUTURES_LIST = [
   `wifi`,
   `dishwasher`,
   `parking`,
@@ -25,12 +25,12 @@ const featuresList = [
   `elevator`,
   `conditioner`
 ];
-const photosList = [
+const PHOTOS_LIST = [
   `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
 ];
-const typesList = [
+const TYPES_LIST = [
   `palace`,
   `flat`,
   `house`,
@@ -39,16 +39,15 @@ const typesList = [
 
 document.querySelector(`.map`).classList.remove(`map--faded`);
 
-const getRandomNumber = function (min, max) {
-  const rand = min - 0.5 + Math.random() * (max - min + 1);
-  return Math.round(rand);
+const getRandomNumber = (max, min = 0) => {
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
-const getRandomElement = function (elements) {
+const getRandomElement = (elements) => {
   return elements[Math.floor(Math.random() * elements.length)];
 };
 
-const getRandomArray = function (primaryElements) {
+const getRandomArray = (primaryElements) => {
   const copyElements = primaryElements.slice();
   let j;
   let temp;
@@ -61,48 +60,44 @@ const getRandomArray = function (primaryElements) {
   return copyElements.slice(getRandomNumber(0, copyElements.length));
 };
 
-const getPin = function (templateObject) {
-  const similarPinTemplate = document.querySelector(`#pin`)
-  .content
-  .querySelector(`.map__pin`);
+const getPin = (templateObject) => {
+  const similarPinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
 
   const pinElement = similarPinTemplate.cloneNode(true);
 
-  pinElement.style.left = templateObject.location.x - (PIN_WIDTH / 2) + `px`;
-  pinElement.style.top = templateObject.location.y - PIN_HEIGHT + `px`;
+  pinElement.style.left = `${templateObject.location.x - (PIN_WIDTH / 2)}px`;
+  pinElement.style.top = `${templateObject.location.y - PIN_HEIGHT}px`;
   pinElement.querySelector(`img`).src = templateObject.author.avatar;
   pinElement.querySelector(`img`).alt = templateObject.offer.title;
 
   return pinElement;
 };
 
-const generatePins = function (amount) {
+const generatePins = (amount) => {
   const pinsList = [];
 
   for (let i = 1; i <= amount; i++) {
-    const x = getRandomNumber(MIN_X, MAX_X);
-    const y = getRandomNumber(MIN_Y, MAX_Y);
 
     pinsList.push({
       "author": {
-        "avatar": `img/avatars/user0` + i + `.png`
+        "avatar": `img/avatars/user0${i}.png`
       },
       "offer": {
         "title": `Уютная студия у метро`,
-        "address": x + `,` + y,
+        "address": `${getRandomNumber(MIN_X, MAX_X)}, ${getRandomNumber(MIN_Y, MAX_Y)}`,
         "price": 10000,
-        "type": getRandomElement(typesList),
+        "type": getRandomElement(TYPES_LIST),
         "rooms": 1,
         "guests": 2,
-        "checkin": getRandomElement(checkinOptions),
-        "checkout": getRandomElement(checkoutOptions),
-        "features": getRandomArray(featuresList),
+        "checkin": getRandomElement(CHEKIN_OPTIONS),
+        "checkout": getRandomElement(CHEKOUT_OPTIONS),
+        "features": getRandomArray(FEAUTURES_LIST),
         "description": `Хорошая квартира-студия для комфортного проживания`,
-        "photos": getRandomArray(photosList)
+        "photos": getRandomArray(PHOTOS_LIST)
       },
       "location": {
-        "x": x,
-        "y": y
+        "x": `${getRandomNumber(MIN_X, MAX_X)}`,
+        "y": `${getRandomNumber(MIN_Y, MAX_Y)}`,
       }
     });
   }
